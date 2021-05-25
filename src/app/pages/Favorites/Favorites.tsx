082942cd60252ea9d2a3394de5f4b22edbaf2333
Tomshsh <shmtomer@gmail.com>
2021-05-26 00:03:33 +0300

    favorites grid layout

--- src/app/pages/Favorites/Favorites.tsx
+++ src/app/pages/Favorites/Favorites.tsx
@@ -2,33 +2,43 @@ import { Container, Grid, makeStyles } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles(theme => ({
-  favorites:{},
-  favoritesContainer:{},
+  favorites: {
+    padding: '40px 0'
+  },
+  favoritesContainer: {},
+  favoriteItem: {
+    borderRadius: 10,
+    height: 300,
+    boxShadow: "0 20px 43px hsla(0,0%,39.2%,0.05)",
+    backgroundColor: theme.palette.primary.main,
+    textAlign: 'center',
+    margin: 30 
+  }
 
 }))
 
 const favorites = [
   {
-    city:'London',
+    city: 'London',
     temp: '36',
     icon: '../../../icons/MostlyCloudy.svg',
     description: 'mostly cloudy',
     lastUpdated: '17:00'
   },
   {
-    city:'New York',
+    city: 'New York',
     temp: '30',
     icon: '../../../icons/Partlycloudy(night).svg',
     description: 'partly cloudy',
     lastUpdated: '17:00'
-  },{
-    city:'Bangkok',
+  }, {
+    city: 'Bangkok',
     temp: '27',
     icon: '../../../icons/Showers(day).svg',
     description: 'showers',
     lastUpdated: '17:00'
-  },{
-    city:'Shanghai',
+  }, {
+    city: 'Shanghai',
     temp: '36',
     icon: '../../../icons/Sunny.svg',
     description: 'sunny',
@@ -43,9 +53,11 @@ const Favorites: React.FC = () => {
   return (
     <div className={classes.favorites}>
       <Container className={classes.favoritesContainer}>
-        <Grid container >
-          {favorites.map((f,i) => (
-            <Grid item xs></Grid>
+        <Grid container justify="center">
+          {favorites.map((f, i) => (
+            <Grid item xs={12} md={6}  >
+              <div className={classes.favoriteItem}></div>
+            </Grid>
           ))}
         </Grid>
       </Container>
