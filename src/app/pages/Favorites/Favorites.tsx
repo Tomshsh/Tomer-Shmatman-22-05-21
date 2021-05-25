5d1a45573da52f7bf694ab30a500bc03357df917
Tomshsh <shmtomer@gmail.com>
2021-05-25 23:19:26 +0300

    favorites component and dummy data

--- src/app/pages/Favorites/Favorites.tsx
+++ src/app/pages/Favorites/Favorites.tsx
@@ -7,6 +7,35 @@ const useStyles = makeStyles(theme => ({
 
 }))
 
+const favorites = [
+  {
+    city:'London',
+    temp: '36',
+    icon: '../../../icons/MostlyCloudy.svg',
+    description: 'mostly cloudy',
+    lastUpdated: '17:00'
+  },
+  {
+    city:'New York',
+    temp: '30',
+    icon: '../../../icons/Partlycloudy(night).svg',
+    description: 'partly cloudy',
+    lastUpdated: '17:00'
+  },{
+    city:'Bangkok',
+    temp: '27',
+    icon: '../../../icons/Showers(day).svg',
+    description: 'showers',
+    lastUpdated: '17:00'
+  },{
+    city:'Shanghai',
+    temp: '36',
+    icon: '../../../icons/Sunny.svg',
+    description: 'sunny',
+    lastUpdated: '17:00'
+  }
+]
+
 const Favorites: React.FC = () => {
 
   const classes = useStyles()
@@ -15,7 +44,9 @@ const Favorites: React.FC = () => {
     <div className={classes.favorites}>
       <Container className={classes.favoritesContainer}>
         <Grid container >
-
+          {favorites.map((f,i) => (
+            <Grid item xs></Grid>
+          ))}
         </Grid>
       </Container>
     </div>
