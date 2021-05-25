5d2ae2a8f9cdd3b7acc37dfad1f91bacd61c371f
Tomshsh <shmtomer@gmail.com>
2021-05-25 16:46:22 +0300

    a start for Favorites page

--- src/app/pages/Favorites/Favorites.tsx
+++ src/app/pages/Favorites/Favorites.tsx
@@ -1,10 +1,25 @@
+import { Container, Grid, makeStyles } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles(theme => ({
+  favorites:{},
+  favoritesContainer:{},
 
-const Favorites: React.FC = () => (
-  <div>
-    Favorites Component
-  </div>
-);
+}))
+
+const Favorites: React.FC = () => {
+
+  const classes = useStyles()
+
+  return (
+    <div className={classes.favorites}>
+      <Container className={classes.favoritesContainer}>
+        <Grid container >
+
+        </Grid>
+      </Container>
+    </div>
+  )
+};
 
 export default Favorites;
