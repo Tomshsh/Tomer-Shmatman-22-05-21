cf92fecef1454a413cac9b14c7abb9d1dde26c39
Tomshsh <shmtomer@gmail.com>
2021-05-26 14:31:00 +0300

    favorites page responsive styling

--- src/app/pages/Favorites/Favorites.tsx
+++ src/app/pages/Favorites/Favorites.tsx
@@ -1,5 +1,6 @@
-import { Container, Grid, makeStyles } from '@material-ui/core';
+import { Container, Grid, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import icon from '../../../icons/MostlyCloudy.svg'
 
 const useStyles = makeStyles(theme => ({
   favorites: {
@@ -9,10 +10,30 @@ const useStyles = makeStyles(theme => ({
   favoriteItem: {
     borderRadius: 10,
     height: 300,
+    maxWidth: 600,
     boxShadow: "0 20px 43px hsla(0,0%,39.2%,0.05)",
     backgroundColor: theme.palette.primary.main,
-    textAlign: 'center',
-    margin: 30 
+    display: 'flex',
+    margin: 30,
+    padding: theme.spacing(4),
+    [theme.breakpoints.down("sm")]: {
+      margin: '30px auto',
+    }
+  },
+  description: {
+    flex: '0 0 50%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  icons: {
+    flex: '0 0 50%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems:'right',
+
+    '& img':{height:'50%'}
   }
 
 }))
@@ -21,26 +42,26 @@ const favorites = [
   {
     city: 'London',
     temp: '36',
-    icon: '../../../icons/MostlyCloudy.svg',
+    icon: '/src/icons/MostlyCloudy.svg',
     description: 'mostly cloudy',
     lastUpdated: '17:00'
   },
   {
     city: 'New York',
     temp: '30',
-    icon: '../../../icons/Partlycloudy(night).svg',
+    icon: 'src/icons/Partlycloudy(night).svg',
     description: 'partly cloudy',
     lastUpdated: '17:00'
   }, {
     city: 'Bangkok',
     temp: '27',
-    icon: '../../../icons/Showers(day).svg',
+    icon: 'src/icons/Showers(day).svg',
     description: 'showers',
     lastUpdated: '17:00'
   }, {
     city: 'Shanghai',
     temp: '36',
-    icon: '../../../icons/Sunny.svg',
+    icon: 'src/icons/Sunny.svg',
     description: 'sunny',
     lastUpdated: '17:00'
   }
@@ -55,8 +76,22 @@ const Favorites: React.FC = () => {
       <Container className={classes.favoritesContainer}>
         <Grid container justify="center">
           {favorites.map((f, i) => (
-            <Grid item xs={12} md={6}  >
-              <div className={classes.favoriteItem}></div>
+            <Grid item xs={12} md={6} >
+              <div className={classes.favoriteItem}>
+                <div className={classes.description}>
+                  <div>
+                    <Typography variant="h4">{f.city}</Typography>
+                    <Typography variant="caption">{f.lastUpdated}</Typography>
+                  </div>
+                  <div>
+                    <Typography variant="h3">{f.temp}°</Typography>
+                    <Typography variant="caption">{f.description}</Typography>
+                  </div>
+                </div>
+                <div className={classes.icons}>
+                  <img src={icon} alt="" />
+                </div>
+              </div>
             </Grid>
           ))}
         </Grid>
