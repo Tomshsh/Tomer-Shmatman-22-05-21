69a286e29dc65667a48b483588e5e4fec3d346d2
Tomshsh <shmtomer@gmail.com>
2021-05-26 12:09:50 +0300

    broke NextDays into 2 components, now rendering a list of NextDay. NextDays made responsive

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,18 +1,12 @@
-import { Container, createMuiTheme, InputAdornment, makeStyles, TextField, ThemeProvider, Typography } from '@material-ui/core';
+import { Container, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import CurrentWeather from './CurrentWeather/CurrentWeather';
 import NextDays from './NextDays/NextDays';
-import {Search} from '@material-ui/icons'
 
 const useStyles = makeStyles(theme => ({
   weatherHead: {
     position: 'relative',
     padding: '40px 0'
-  },
-  background: {
-    position: 'absolute',
-    // background: 'transparent linear-gradient(198deg, #00cdec, #00449b) 0 0 no-repeat padding-box',
-    top: 0, bottom: 0, right: 0, left: 0
   },
   weatherHeadContainer: {
     position: 'relative'
@@ -28,7 +22,6 @@ const Home: React.FC = () => {
 
   return (
     <div className={classes.weatherHead}>
-      {/* <div className={classes.background}></div> */}
       <Container className={classes.weatherHeadContainer} maxWidth="lg">
         <CurrentWeather />
       </Container>
--- src/app/pages/Home/NextDays/NextDays.tsx
+++ src/app/pages/Home/NextDays/NextDays.tsx
@@ -1,85 +1,29 @@
-import { Divider, Grid, makeStyles, Typography } from '@material-ui/core'
+import {Grid, makeStyles } from '@material-ui/core'
 import React from 'react'
 import dummyTemps from '../../../dummyData/dummyTemps'
-import img from "../../../../icons/IntermittentClouds(day).4a7ba1cb.svg"
+import NextDay from './NextDay'
 
 const useStyles = makeStyles(theme => ({
     nextDays: {
-        borderRadius: 10,
-        height: 300,
-        boxShadow: "0 20px 43px hsla(0,0%,39.2%,0.05)",
-        backgroundColor: theme.palette.primary.main,
-        textAlign: 'center'
+        textAlign: 'center',
+        [theme.breakpoints.up('md')]: {
+            borderRadius: 10,
+            overflow:'hidden'
+        }
     },
-    daysGrid: {
-        height: '100%'
-    },
-    dayItem: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '25px 15px'
-    },
-    tempBar: {
-        width: "5vw",
-        backgroundColor: theme.palette.success.light,
-        height: 40,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-
-        '&.max': {
-            justifyContent: 'start',
-        },
-        '&.min': {
-            justifyContent: 'end'
-        },
-    },
-    minMaxItem: {
-        display: 'flex',
-        alignItems: 'baseline',
-
-        '& h4, h5': { fontWeight: 'bold', },
-        '& h5': { color: theme.palette.text.secondary }
-    }
+    
 }))
 
 const NextDays = () => {
 
-    const getDayAbbr = (date: string) => {
-        const dateFormat = new Date(date)
-        return new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(dateFormat)
-    }
-
-    const getDate = (date: string) => {
-        const dateFormat = new Date(date)
-        // @ts-ignore
-        return new Intl.DateTimeFormat('en-US', { dateStyle: 'medium' }).format(dateFormat)
-    }
 
     const classes = useStyles()
 
     return (
         <div className={classes.nextDays}>
-            <Grid container className={classes.daysGrid} >
+            <Grid container >
                 {dummyTemps.DailyForecasts.map((d, i) => (
-                    <>
-                        {i > 0 && <Divider orientation="vertical" flexItem />}
-                        <Grid item key={d.Date} xs className={classes.dayItem}>
-                            <div>
-                                <Typography variant="h5">{getDayAbbr(d.Date)}</Typography>
-                                <Typography variant="caption">{getDate(d.Date)}</Typography>
-                            </div>
-                            <div>
-                                <img src={img} alt="" style={{ marginRight: '20px', display:'block' }} />
-                            </div>
-                            <div className={classes.minMaxItem}>
-                                <Typography variant="h4">{d.Temperature.Maximum.Value}°</Typography>
-                                <Typography variant="h5">/{d.Temperature.Maximum.Value}°</Typography>
-                            </div>
-                        </Grid>
-                    </>
+                    <NextDay day={d} iterator={i} key={d.Date}/>
                 ))}
             </Grid>
         </div>
